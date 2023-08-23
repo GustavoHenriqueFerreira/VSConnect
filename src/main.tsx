715f491e04cde202f5b39eee136b6e3da3e07851
GustavoHenriqueFerreira <gustavohferreiraalves@gmail.com>
2023-08-22 21:53:39 -0300

    feat: Cadastro de usuario

--- src/main.tsx
+++ src/main.tsx
@@ -8,13 +8,13 @@ import {
 import Header from './components/Header/index.tsx';
 import Home from './pages/Home/index.tsx';
 import ListaServicos from './pages/ListaServicos/index.tsx';
-import Cadastro from './pages/Cadastro/index.tsx';
 import CadastroServico from './pages/CadastroServico/index.tsx';
 import ListaDevs from './pages/ListaDevs/index.tsx';
 import Login from './pages/Login/index.tsx';
 import './index.css';
 import PerfilUsuario from './pages/PerfilUsuario/index.tsx';
 import VisualizarServico from './pages/VisualizarServico/index.tsx';
+import CadastroUsuario from './pages/CadastroUsuario/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -23,7 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/lista/servicos' element={<ListaServicos />} />
-        <Route path='/cadastro/usuario' element={<Cadastro />} />
+        <Route path='/cadastro/usuario' element={<CadastroUsuario />} />
         <Route path='/cadastro/servicos' element={<CadastroServico />} />
         <Route path='/lista/devs' element={<ListaDevs />} />
         <Route path='/login' element={<Login />} />
